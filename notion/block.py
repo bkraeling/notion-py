1b762e1d2a86c40ace76d9e96a17651fcc3e457d
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2020-02-29 15:06:42 -0500

    Remove slow iteration for getting the collection view
    https://github.com/jamalex/notion-py/pull/96/commits/05f8c60921b624c8aaf694f3d535d8f9ddef62d4#r365629145

--- notion/block.py
+++ notion/block.py
@@ -735,22 +735,9 @@ class CollectionViewBlockViews(Children):
     child_list_key = "view_ids"
 
     def _get_block(self, view_id):
-
-        view = self._client.get_collection_view(
+        return self._client.get_collection_view(
             view_id, collection=self._parent.collection
         )
-
-        i = 0
-        while view is None:
-            i += 1
-            if i > 20:
-                return None
-            time.sleep(0.1)
-            view = self._client.get_collection_view(
-                view_id, collection=self._parent.collection
-            )
-
-        return view
 
     def add_new(self, view_type="table"):
         if not self._parent.collection:
